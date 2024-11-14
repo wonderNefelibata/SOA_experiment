7ac914332b7948aabfa4fcfa66e246cc4cefcb89
WONDER <909785560@qq.com>
2024-11-14 10:28:32 +0800

    init the proj

---
+++ src/main/java/org/csu/soaex2/SoaEx2Application.java
@@ -0,0 +1,13 @@
+package org.csu.soaex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaEx2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoaEx2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/csu/soaex2/SoaEx2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.csu.soaex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoaEx2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
