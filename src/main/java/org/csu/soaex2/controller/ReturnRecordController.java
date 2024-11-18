e594c34fa0245ce8a50193c34dd216dfe248c328
WONDER <909785560@qq.com>
2024-11-18 11:37:03 +0800

    验证CommonResponse可用并且提供GreetingController示例

--- src/main/java/org/csu/soaex2/controller/ReturnRecordController.java
+++ src/main/java/org/csu/soaex2/controller/ReturnRecordController.java
@@ -12,49 +12,49 @@ import java.util.List;
 @RestController
 @RequestMapping("/return_records")
 public class ReturnRecordController {
-    @Autowired
-    private ReturnRecordService returnRecordService;
-
-    @GetMapping
-    public ResponseEntity<CommonResponse<List<ReturnRecord>>> getAllReturnRecords() {
-        List<ReturnRecord> records = returnRecordService.list();
-        if (records.isEmpty()) {
-            return CommonResponse.error("没有找到归还记录");
-        } else {
-            return CommonResponse.success(records);
-        }
-    }
-
-    @GetMapping("/{record_id}")
-    public ResponseEntity<CommonResponse<ReturnRecord>> getReturnRecordById(@PathVariable String record_id) {
-        ReturnRecord record = returnRecordService.getReturnRecordById(record_id);
-        if (record == null) {
-            return CommonResponse.error("归还记录不存在");
-        } else {
-            return CommonResponse.success(record);
-        }
-    }
-
-    @PostMapping
-    public ResponseEntity<CommonResponse<ReturnRecord>> createReturnRecord(@RequestBody ReturnRecord returnRecord) {
-        boolean success = returnRecordService.createReturnRecord(returnRecord);
-        if (success) {
-            return CommonResponse.created(returnRecord);
-        } else {
-            return CommonResponse.error("创建归还记录失败");
-        }
-    }
-
-    @PutMapping("/{record_id}")
-    public ResponseEntity<CommonResponse<ReturnRecord>> updateReturnRecord(@PathVariable String record_id, @RequestBody ReturnRecord ReturnRecord) {
-        ReturnRecord.setRecordId(record_id);
-        boolean success = returnRecordService.updateReturnRecord(record_id, ReturnRecord);
-
-        if (success) {
-            return CommonResponse.success(ReturnRecord);
-        } else {
-            return CommonResponse.error("更新归还记录失败");
-        }
-    }
+//    @Autowired
+//    private ReturnRecordService returnRecordService;
+//
+//    @GetMapping
+//    public ResponseEntity<CommonResponse<List<ReturnRecord>>> getAllReturnRecords() {
+//        List<ReturnRecord> records = returnRecordService.list();
+//        if (records.isEmpty()) {
+//            return CommonResponse.error("没有找到归还记录");
+//        } else {
+//            return CommonResponse.success(records);
+//        }
+//    }
+//
+//    @GetMapping("/{record_id}")
+//    public ResponseEntity<CommonResponse<ReturnRecord>> getReturnRecordById(@PathVariable String record_id) {
+//        ReturnRecord record = returnRecordService.getReturnRecordById(record_id);
+//        if (record == null) {
+//            return CommonResponse.error("归还记录不存在");
+//        } else {
+//            return CommonResponse.success(record);
+//        }
+//    }
+//
+//    @PostMapping
+//    public ResponseEntity<CommonResponse<ReturnRecord>> createReturnRecord(@RequestBody ReturnRecord returnRecord) {
+//        boolean success = returnRecordService.createReturnRecord(returnRecord);
+//        if (success) {
+//            return CommonResponse.created(returnRecord);
+//        } else {
+//            return CommonResponse.error("创建归还记录失败");
+//        }
+//    }
+//
+//    @PutMapping("/{record_id}")
+//    public ResponseEntity<CommonResponse<ReturnRecord>> updateReturnRecord(@PathVariable String record_id, @RequestBody ReturnRecord ReturnRecord) {
+//        ReturnRecord.setRecordId(record_id);
+//        boolean success = returnRecordService.updateReturnRecord(record_id, ReturnRecord);
+//
+//        if (success) {
+//            return CommonResponse.success(ReturnRecord);
+//        } else {
+//            return CommonResponse.error("更新归还记录失败");
+//        }
+//    }
 
 }
